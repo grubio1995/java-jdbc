b357cd5536096acb2f006de22b6929c40a9c841b
Gabriel Rubio <gabrielpinhatarubio@gmail.com>
2022-02-10 21:43:38 -0300

    Listagem com Statement

--- src/TestaConexao.java
+++ src/TestaConexao.java
@@ -7,9 +7,6 @@ public class TestaConexao {
 	public static void main(String[] args) throws SQLException {
 		
 		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "1234");
-		
-		System.out.println("Fechando conexão!!");
-		
 		connection.close();
 	}
 }
