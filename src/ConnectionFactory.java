88d43355bf4957dc4845902d31409cc3e1f4e3ac
Gabriel Rubio <gabrielpinhatarubio@gmail.com>
2022-03-08 22:39:04 -0300

    Testando o pool de conexões

--- src/ConnectionFactory.java
+++ src/ConnectionFactory.java
@@ -15,6 +15,8 @@ public class ConnectionFactory {
 		comboPooledDataSource.setUser("root");
 		comboPooledDataSource.setPassword("1234");
 
+		comboPooledDataSource.setMaxPoolSize(15);
+		
 		this.dataSource = comboPooledDataSource;
 	}
 	
