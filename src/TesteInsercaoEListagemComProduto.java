9d8215ebc9945f973f85a4fcd5143904808089e5
Gabriel Rubio <gabrielpinhatarubio@gmail.com>
2022-03-10 22:54:38 -0300

    Listando no ProdutoDAO

--- src/TesteInsercaoEListagemComProduto.java
+++ src/TesteInsercaoEListagemComProduto.java
@@ -2,20 +2,25 @@
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
 import br.com.alura.jdbc.ConnectionFactory;
 import dao.ProdutoDao;
 import modelo.Produto;
 
-public class TesteInsercaoComProduto {
+public class TesteInsercaoEListagemComProduto {
 
 	public static void main(String[] args) throws SQLException{	
 
 		Produto comoda = new Produto("Cômoda", "Cômoda Vertical");
 
 		try(Connection connection = new ConnectionFactory().recuperarConexao()){
-			ProdutoDao persistenciaProduto = new ProdutoDao(connection);
-			persistenciaProduto.salvarProduto(comoda);
+			ProdutoDao produtoDao = new ProdutoDao(connection);
+			produtoDao.salvar(comoda);
+			
+			List<Produto> listaProdutos = produtoDao.listar();
+			listaProdutos.forEach(System.out::println);
+				
 		}
 	}
 }
--- src/dao/ProdutoDao.java
+++ src/dao/ProdutoDao.java
@@ -5,6 +5,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import modelo.Produto;
 
@@ -16,7 +18,7 @@ public class ProdutoDao {
 		this.connection = connection;
 	}
 
-	public void salvarProduto(Produto produto) throws SQLException {
+	public void salvar(Produto produto) throws SQLException {
 
 		String sql = "INSERT INTO PRODUTO (NOME,DESCRICAO) VALUES (?,?)";
 
@@ -36,5 +38,27 @@ public class ProdutoDao {
 		}
 
 		System.out.println(produto);
+	}
+	
+	public List<Produto> listar() throws SQLException {
+
+		
+		List<Produto> produtos = new ArrayList<>();
+		
+		String sql = "SELECT * FROM PRODUTO";
+
+		try (PreparedStatement pstm = connection.prepareStatement(sql)) {
+
+			pstm.execute();
+
+			try (ResultSet rst = pstm.getResultSet()) {
+
+				while (rst.next()) {
+					produtos.add(new Produto(rst.getInt(1),rst.getString(2), rst.getString(3)));
+				}
+			}
+		}
+		
+		return produtos;
 	}
 }
